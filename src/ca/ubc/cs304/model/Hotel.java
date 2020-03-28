a04e37c0698e0bd321c4e99515318197a82e0308
Aviral Saxena <aviral1231@gmail.com>
2020-03-27 21:08:11 -0700

    Classes for SQL Tables (#1)

---
+++ src/ca/ubc/cs304/model/Hotel.java
@@ -0,0 +1,25 @@
+package ca.ubc.cs304.model;
+
+public class Hotel {
+	private final char[] address;
+	private final char[] name;
+	private final int capacity;
+
+	public Hotel(char[] address, char[] name, int capacity) {
+		this.address = address;
+		this.name = name;
+		this.capacity = capacity;
+	}
+
+	public char[] getAddress() {
+		return address;
+	}
+
+	public char[] getName() {
+		return name;
+	}
+
+	public int getCapacity() {
+		return capacity;
+	}
+}
