ff4f49e78ae0a34c52e8b3ea6d9b7bad1c2e3f3a
TheAvi123 <aviral1231@gmail.com>
2020-04-02 17:06:45 -0700

    Database Connection Handler Updates

--- src/ca/ubc/cs304/database/DatabaseConnectionHandler.java
+++ src/ca/ubc/cs304/database/DatabaseConnectionHandler.java
@@ -6,7 +6,6 @@ import ca.ubc.cs304.model.BranchModel;
 import ca.ubc.cs304.model.Room;
 import ca.ubc.cs304.modelInterface.Entity;
 import ca.ubc.cs304.modelInterface.TableHelper;
-import javafx.embed.swt.SWTFXUtils;
 import org.json.JSONObject;
 
 /**
